eaa4c4130383e7ab74d69cdeabb0c6b49edcb76a
SchelmInsel5123 <76526609+SchelmInsel5123@users.noreply.github.com>
2026-03-08 20:16:06 +0100

    Updated ProductDetail.jsx

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
